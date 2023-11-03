d8fa94a871939bec334b25c2a65a9f6d64aa7d5b
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2023-11-03 02:29:35 +0100

    Add branch and bound as an option of solving tsp

--- src/main/java/TSP/implementations/forhuman/HumanAlgorithmProvider.java
+++ src/main/java/TSP/implementations/forhuman/HumanAlgorithmProvider.java
@@ -2,6 +2,7 @@ package TSP.implementations.forhuman;
 
 import TSP.algorithms.Algorithm;
 import TSP.algorithms.AlgorithmProvider;
+import TSP.algorithms.BranchAndBound.BranchAndBound;
 import TSP.algorithms.BruteForce;
 
 import java.util.Scanner;
@@ -14,13 +15,17 @@ public class HumanAlgorithmProvider implements AlgorithmProvider {
 
             System.out.println("Wybierz algorytm");
             System.out.println("1. BruteForce");
-            System.out.println("2. Wyjście");
+            System.out.println("2. Branch&Bound");
+            System.out.println("3. Wyjście");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1 -> {
                     return new BruteForce();
                 }
                 case 2 -> {
+                    return new BranchAndBound();
+                }
+                case 3 -> {
                     return null;
                 }
                 default -> System.out.println("Nieprawidłowy wybór. Wybierz ponownie.");
