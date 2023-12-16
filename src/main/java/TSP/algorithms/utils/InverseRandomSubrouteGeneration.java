ed44dfd007f5bad7ca5b3e1caab23c182c4f4f57
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2023-12-16 11:59:22 +0100

    Add inverse neighbor generation

---
+++ src/main/java/TSP/algorithms/utils/InverseRandomSubrouteGeneration.java
@@ -0,0 +1,45 @@
+package TSP.algorithms.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class InverseRandomSubrouteGeneration implements NeighborGeneration {
+    @Override
+    public List<List<Integer>> generateNeighbors(List<Integer> solution, int numberOfNeightbors) {
+        Random r = new Random();
+        List<List<Integer>> neighbours = new ArrayList<>();
+
+        for(int i = 0 ; i < numberOfNeightbors; i++) {
+            List<Integer> neighbour = new ArrayList<>(solution);
+            int a = 0;
+            int b = 0;
+            while (a == b) {
+                a = r.nextInt(solution.size());
+                b = r.nextInt(solution.size());
+            }
+
+            if (a > b) {
+                int buff = b;
+                b = a;
+                a = buff;
+            }
+
+            while (a < b) {
+                Collections.swap(neighbour, a, b);
+                a++;
+                b--;
+            }
+
+            neighbours.add(neighbour);
+        }
+
+        return neighbours;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
